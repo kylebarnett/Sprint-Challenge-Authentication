c295283039c4fb2c7e0c8960ec184ba3bca7cba7
Kyle Barnett <kbarnett23@gmail.com>
2019-12-14 10:56:59 -0700

    login/register works

--- auth/authenticate-middleware.js
+++ auth/authenticate-middleware.js
@@ -2,7 +2,13 @@
   complete the middleware code to check if the user is logged in
   before granting access to the next middleware/route handler
 */
+const bcrypt = require('bcryptjs');
+const Users = require('../jokes/jokes.model.js');
 
 module.exports = (req, res, next) => {
-  res.status(401).json({ you: 'shall not pass!' });
+  if (req.session && req.session.user) {
+    next();
+  } else {
+    res.status(401).json({ you: 'shall not pass!' });
+  }
 };
